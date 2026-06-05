38a5f1d9c4be3fb97ccd2ac7b3dd0bb7f708f330
d11416214-dotcom <d11416214@gs.takming.edu.tw>
2026-06-05 11:23:27 +0800

    Add files via upload

---
+++ D11416214_W12-1/Class1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Data;
+using System.Numerics;
+using System.Reflection;
+
+public class Student
+{
+	public int Mid;
+	public int Final;
+	public Student(int m,int f)
+	{
+		Mid = m;
+		Final = f;
+	}
+	public float Plus(float a,float b)
+	{
+		float temp;
+		temp = (a + b) / 2.0f;
+		return temp;
+	}
+	public static Student operator +(Student a, Student b) 
+	{
+		Student temp=new Student(0,0);
+		temp.Mid = (a.Mid + b.Mid) / 2;
+		temp.Final = (a.Final + b.Final) / 2;
+		return temp;
+	}
+}
---
+++ D11416214_W12-1/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace D11416214_W12_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            Student s1 = new Student(60,70);
+            Console.WriteLine(s1.Mid);
+            Console.WriteLine(s1.Final);
+            Console.WriteLine("------------------------");
+
+            Student s2 = new Student(70, 80);
+            Console.WriteLine(s2.Mid);
+            Console.WriteLine(s2.Final);
+            Console.WriteLine("------------------------");
+            */
+            //Console.WriteLine(s2.Plus(s1.Mid, s1.Final));
+            //Console.WriteLine(s2.Plus(s1.Final,s1.Mid));
+            //Student savg;
+            //savg = s1 + s2;
+            //Console.WriteLine(savg.Mid);
+            //Console.WriteLine(savg.Final);
+            Student s1 = new Student(60, 70);
+            Student s2 = new Student(75, 80);
+            Student savg = s1 + s2;
+            Console.WriteLine(savg.Mid + "" + savg.Final);
+        }
+    }
+}
